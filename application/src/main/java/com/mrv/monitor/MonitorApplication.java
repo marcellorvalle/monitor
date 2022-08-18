3007d08943e5c538fa96ee33351d5be841264bc5
Marcello <marcellorvalle@gmail.com>
2022-08-18 18:45:20 -0300

    ajustes

--- application/src/main/java/com/mrv/monitor/MonitorApplication.java
+++ application/src/main/java/com/mrv/monitor/MonitorApplication.java
@@ -1,6 +1,5 @@
 package com.mrv.monitor;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
