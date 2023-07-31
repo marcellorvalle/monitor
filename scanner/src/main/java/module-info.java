00af1e51b207cb6838ad24500b42d209f0bf5ac5
Marcello <marcellorvalle@gmail.com>
2023-07-31 19:09:43 -0300

    Solved RestTemplateBuilder dependency.

--- scanner/src/main/java/module-info.java
+++ scanner/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.mrv.monitor.scanner {
-    requires lombok;
+    //    requires lombok;
     requires spring.webflux;
-    requires spring.context;
+    //    requires spring.context;
     requires spring.web;
-    requires org.slf4j;
+    //    requires org.slf4j;
     //exports com.mrv.monitor.scanner.model;
     //exports com.mrv.monitor.scanner.service;
 }
